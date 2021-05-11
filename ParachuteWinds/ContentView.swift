cc81836a0f9c1b8ec7ea68594da15a3000fb0120
Eric Kitaif <eric.kitaif@gmail.com>
2021-05-10 20:44:11 -0500

    Initial Commit

---
+++ ParachuteWinds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParachuteWinds
+//
+//  Created by Eric Kitaif on 5/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ParachuteWinds/ParachuteWindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParachuteWindsApp.swift
+//  ParachuteWinds
+//
+//  Created by Eric Kitaif on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ParachuteWindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
